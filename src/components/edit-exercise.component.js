6e37ee0996bd12ce434666d154d6bd63f05a4862
Debrup Mondal <debrup.official.365@gmail.com>
2021-03-04 13:45:07 +0530

    frontend done

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+
+export default class EditExercises extends Component {
+ render(){
+     return(
+
+        <div>
+            <p>Edit ExerciseWorks</p>
+        </div>
+     )
+      
+ }
+
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+
+export default class ExercisesList extends Component {
+ render(){
+     return(
+
+        <div>
+            <p>Exercise List Works</p>
+        </div>
+     )
+      
+ }
+
+}
